34cca4b3e2c574e55e57cb1173cba55073d7243b
贤壬黄 <1459562870@qq.com>
2021-06-15 15:34:38 +0800

    备份

--- campus-wall-web/src/main/java/com/nf/campuswall/controller/AddUserByEmailPassword.java
+++ campus-wall-web/src/main/java/com/nf/campuswall/controller/AddUserByEmailPassword.java
@@ -1,6 +1,8 @@
 package com.nf.campuswall.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nf.campuswall.dao.UserDao;
+import com.nf.campuswall.dao.impl.UserDaoImpl;
 import com.nf.campuswall.entity.UserEntity;
 
 import javax.servlet.ServletException;
@@ -17,6 +19,22 @@ import java.util.Map;
 public class AddUserByEmailPassword extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader br = req.getReader();
+        String str, wholeStr = "";
+        while ((str = br.readLine()) != null) {
+            wholeStr += str;
+        }
+
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final Map map = objectMapper.readValue(wholeStr, Map.class);
+
+
+        String json = objectMapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(map);
+        final UserEntity userEntity = objectMapper.readValue(json, UserEntity.class);
+
+        final int i = new UserDaoImpl().userEmailRegistrationByEmailPassword(userEntity);
+
 
     }
 }
--- campus-wall-web/src/main/java/com/nf/campuswall/controller/UserLoginByPasswordEmail.java
+++ campus-wall-web/src/main/java/com/nf/campuswall/controller/UserLoginByPasswordEmail.java
@@ -3,18 +3,20 @@ package com.nf.campuswall.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nf.campuswall.dao.impl.UserDaoImpl;
 import com.nf.campuswall.entity.UserEntity;
+import vo.ResultVo;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Map;
 
-@WebServlet("/PhoneEnroll")
-public class AddUserByNamePassword extends HttpServlet {
+@WebServlet("/loginUser")
+public class UserLoginByPasswordEmail extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         BufferedReader br = req.getReader();
@@ -31,7 +33,15 @@ public class AddUserByNamePassword extends HttpServlet {
                 .writeValueAsString(map);
         final UserEntity userEntity = objectMapper.readValue(json, UserEntity.class);
 
-        final int i = new UserDaoImpl().userPhoneRegistrationByNamePassword(userEntity);
+        final UserEntity user = new UserDaoImpl().loginByEmailOrPhoneAndPassword(userEntity);
+
+        if (user.getUserEmail() != null && user.getUserPassword() != null || user.getUserPassword() != null) {
+            HttpSession session = req.getSession();
+            session.setAttribute("user", user);
+        }
+
+        resp.getWriter().write(new ObjectMapper().writeValueAsString(ResultVo.fail("账号或密码错误", user.getUserName())));
+
 
     }
 }
